f14a927df3d074790b64a494f67958260ffb571e
Yuliia Ataman <47396774+chandrashka@users.noreply.github.com>
2023-12-21 22:03:26 +0100

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,9 +1,8 @@
--- процедура, яка оновлює дані у таблиці Track
-CREATE OR REPLACE PROCEDURE update_track_energy_percent(track_id INTEGER, new_energy_percent FLOAT)
+CREATE OR REPLACE PROCEDURE update_track_energy_percent(p_track_id INTEGER, p_new_energy_percent DOUBLE PRECISION)
 AS $$
 BEGIN
     UPDATE Track
-    SET energy_percent = new_energy_percent
-    WHERE track_id = update_track_energy_percent.track_id;
+    SET energy_percent = p_new_energy_percent
+    WHERE track_id = p_track_id;
 END;
 $$ LANGUAGE plpgsql;
