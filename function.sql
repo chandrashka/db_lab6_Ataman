515ce35212932af2329a564fc05bc9f08113500d
Yuliia Ataman <47396774+chandrashka@users.noreply.github.com>
2023-12-21 21:59:07 +0100

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,13 +1,12 @@
---Наприклад, функція, що повертає середнє значення streams для заданого track_id
-CREATE OR REPLACE FUNCTION get_average_streams(track_id INTEGER)
+CREATE OR REPLACE FUNCTION get_average_streams(p_track_id INTEGER)
 RETURNS FLOAT AS $$
 DECLARE
-    avg_streams FLOAT;
+   avg_streams FLOAT;
 BEGIN
-    SELECT AVG(streams) INTO avg_streams
-    FROM Streams
-    WHERE track_id = get_average_streams.track_id;
+   SELECT AVG(streams) INTO avg_streams
+   FROM Streams
+   WHERE track_id = p_track_id;
 
-    RETURN avg_streams;
+   RETURN avg_streams;
 END;
 $$ LANGUAGE plpgsql;
